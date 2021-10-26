ec6384b9b64d0cfb2b112da3ad6445f9f05352f9
2010030183-surya <2010030183@klh.edu.in>
2021-10-26 20:26:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
+    </div>
+  )
+}
